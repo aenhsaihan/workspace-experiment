c14d6ef3106d130f4fa4ee44f99e80ccdd8a34bc
Anar Enhsaihan <anar.enhsaihan@cittacorp.com>
2014-08-13 15:27:56 -0400

    Committing my comments on the actual workspace.

---
+++ FourthProject/FourthProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FourthProject
+//
+//  Created by Anar Enhsaihan on 8/13/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FourthProject/FourthProject/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  FourthProject
+//
+//  Created by Anar Enhsaihan on 8/13/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    //Going to commit this to the local repo
+    
+    //More modification
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
